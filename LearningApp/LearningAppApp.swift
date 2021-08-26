c5709519f15a2faa2c43bf622a378a9a1ddd3a8f
stellalo <stellaylo267@gmail.com>
2021-08-26 08:30:20 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by stella on 2021/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
